7298cba60b31337ba91db53a43d7787de28c6a46
marc antoine de morel <marcantoine.demorel@gmail.com>
2018-05-18 09:13:17 +0200

    commiting for newdayt

--- db/schema.rb
+++ db/schema.rb
@@ -23,8 +23,12 @@ ActiveRecord::Schema.define(version: 2018_05_17_090510) do
 
   create_table "doses", force: :cascade do |t|
     t.string "description"
+    t.bigint "ingredient_id"
+    t.bigint "cocktail_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["cocktail_id"], name: "index_doses_on_cocktail_id"
+    t.index ["ingredient_id"], name: "index_doses_on_ingredient_id"
   end
 
   create_table "ingredients", force: :cascade do |t|
@@ -33,4 +37,6 @@ ActiveRecord::Schema.define(version: 2018_05_17_090510) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "doses", "cocktails"
+  add_foreign_key "doses", "ingredients"
 end
